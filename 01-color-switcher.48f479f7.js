d5690f0fc17530bd59112b399df01017a156c612
Ddemkowicz <114504693+Ddemkowicz@users.noreply.github.com>
2023-04-05 14:38:48 +0000

    Deploying to gh-pages from @ Ddemkowicz/goit-js-hw-11@412bc76c5df661d9a3dcfec23d37b7df6c179e37 🚀

--- 01-color-switcher.48f479f7.js
+++
@@ -1,2 +0,0 @@
-const e=document.querySelector("body"),t=document.querySelector("button[data-start]"),o=document.querySelector("button[data-stop]");function l(){return`#${Math.floor(16777215*Math.random()).toString(16)}`}o.setAttribute("disabled",!0),console.log(t),console.log(o),console.log(l());let d=null;t.addEventListener("click",(()=>{d=setInterval((()=>{e.style.backgroundColor=l()}),1e3),t.disabled=!0,o.disabled=!1})),o.addEventListener("click",(()=>{clearInterval(d),t.disabled=!1,o.disabled=!0}));
-//# sourceMappingURL=01-color-switcher.48f479f7.js.map
